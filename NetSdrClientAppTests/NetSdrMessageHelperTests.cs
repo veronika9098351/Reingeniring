7e77371bcb8d9c87a5c5eecbbb92f95ac3b80238
veronika9098351 <9098351@stud.kai.edu.ua>
2025-10-18 00:38:44 +0300

    Add unit tests for NetSdrMessageHelper methods

--- NetSdrClientAppTests/NetSdrMessageHelperTests.cs
+++ NetSdrClientAppTests/NetSdrMessageHelperTests.cs
@@ -67,3 +67,35 @@ namespace NetSdrClientAppTests
         //TODO: add more NetSdrMessageHelper tests
     }
 }
+[Test]
+public void GetControlItemMessage_ReturnsNotNull()
+{
+    var msg = NetSdrMessageHelper.GetControlItemMessage(
+        NetSdrMessageHelper.MsgTypes.Ack,
+        NetSdrMessageHelper.ControlItemCodes.ReceiverState,
+        new byte[10]
+    );
+    Assert.That(msg, Is.Not.Null);
+}
+
+[Test]
+public void GetDataItemMessage_ReturnsCorrectLength()
+{
+    var msg = NetSdrMessageHelper.GetDataItemMessage(
+        NetSdrMessageHelper.MsgTypes.DataItem2,
+        new byte[100]
+    );
+    Assert.That(msg.Length, Is.GreaterThan(0));
+}
+
+[Test]
+public void ControlItemMessage_HasExpectedHeader()
+{
+    var msg = NetSdrMessageHelper.GetControlItemMessage(
+        NetSdrMessageHelper.MsgTypes.Ack,
+        NetSdrMessageHelper.ControlItemCodes.ReceiverState,
+        new byte[50]
+    );
+    var header = BitConverter.ToUInt16(msg, 0);
+    Assert.That(header, Is.GreaterThan(0));
+}
