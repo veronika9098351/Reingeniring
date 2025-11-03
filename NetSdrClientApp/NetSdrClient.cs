a76034932c1ee01c4fa4d5b72939c104dfce6498
veronika9098351 <9098351@stud.kai.edu.ua>
2025-11-04 00:27:42 +0200

    mist3

--- NetSdrClientApp/NetSdrClient.cs
+++ NetSdrClientApp/NetSdrClient.cs
@@ -16,7 +16,7 @@ namespace NetSdrClientApp
     {
         private ITcpClient _tcpClient;
         private IUdpClient _udpClient;
-        private TaskCompletionSource<byte[]>? responseTaskSource; // ← ВИПРАВЛЕНО
+        private TaskCompletionSource<byte[]>? responseTaskSource; 
 
         public bool IQStarted { get; set; }
 
@@ -39,7 +39,7 @@ namespace NetSdrClientApp
                 var automaticFilterMode = BitConverter.GetBytes((ushort)0).ToArray();
                 var adMode = new byte[] { 0x00, 0x03 };
 
-                //Host pre setup
+           
                 var msgs = new List<byte[]>
                 {
                     NetSdrMessageHelper.GetControlItemMessage(MsgTypes.SetControlItem, ControlItemCodes.IQOutputDataSampleRate, sampleRate),
@@ -113,12 +113,12 @@ namespace NetSdrClientApp
             {
                 foreach (var sample in samples)
                 {
-                    sw.Write((short)sample); //write 16 bit per sample as configured 
+                    sw.Write((short)sample);  
                 }
             }
         }
 
-        private async Task<byte[]?> SendControlMessageAsync(byte[] msg) // ← ВИПРАВЛЕНО
+        private async Task<byte[]?> SendControlMessageAsync(byte[] msg) 
         {
             if (!await EnsureConnectedAsync()) return null;
 
@@ -133,7 +133,6 @@ namespace NetSdrClientApp
 
         private void _tcpClient_MessageReceived(object? sender, byte[] e)
         {
-            //TODO: add Unsolicited messages handling here
             if (responseTaskSource != null)
             {
                 responseTaskSource.SetResult(e);
