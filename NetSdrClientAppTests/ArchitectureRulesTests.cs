72bd92fd964398ed8585a21cab2e862c56954a7c
veronika9098351 <9098351@stud.kai.edu.ua>
2025-11-17 21:03:38 +0200

    Add architecture dependency tests (Lab 5)

---
+++ NetSdrClientAppTests/ArchitectureRulesTests.cs
@@ -0,0 +1,36 @@
+﻿using NetArchTest.Rules;
+using NUnit.Framework;
+
+namespace NetSdrClientAppTests
+{
+    [TestFixture]
+    public class ArchitectureRulesTests
+    {
+        private const string AppNamespace = "NetSdrClientApp";
+        private const string ServerNamespace = "EchoServer";
+
+        [Test]
+        public void ClientApp_ShouldNotDependOn_EchoServer()
+        {
+            var result = Types
+                .InCurrentDomain()
+                .ShouldNot()
+                .HaveDependencyOn(ServerNamespace)
+                .GetResult();
+
+            Assert.That(result.IsSuccessful, Is.True);
+        }
+
+        [Test]
+        public void App_ShouldNotDependOn_TestProject()
+        {
+            var result = Types
+                .InCurrentDomain()
+                .ShouldNot()
+                .HaveDependencyOn("NetSdrClientAppTests")
+                .GetResult();
+
+            Assert.That(result.IsSuccessful, Is.True);
+        }
+    }
+}
