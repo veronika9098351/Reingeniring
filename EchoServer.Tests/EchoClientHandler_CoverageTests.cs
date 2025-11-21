d2f98e7f2ecf194f69f0f2c553ee538353798b21
veronika9098351 <9098351@stud.kai.edu.ua>
2025-11-21 22:56:03 +0200

    fix8

---
+++ EchoServer.Tests/EchoClientHandler_CoverageTests.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace EchoServer.Tests
+{
+    public class EchoClientHandler_CoverageTests
+    {
+        [Test]
+        public async Task Handler_ShouldEchoBack()
+        {
+            var processor = new EchoProcessor();
+            var handler = new EchoClientHandler(processor);
+
+            var server = new TcpListener(IPAddress.Loopback, 0);
+            server.Start();
+            int port = ((IPEndPoint)server.LocalEndpoint).Port;
+
+            var client = new TcpClient();
+            await client.ConnectAsync("127.0.0.1", port);
+
+            var accepted = await server.AcceptTcpClientAsync();
+
+            var stream = client.GetStream();
+            byte[] msg = { 1, 2, 3 };
+
+            await stream.WriteAsync(msg.AsMemory(0, msg.Length));
+
+            await handler.HandleAsync(accepted, default);
+
+            byte[] buf = new byte[100];
+            int read = await stream.ReadAsync(buf.AsMemory(0, buf.Length));
+
+            Assert.That(buf[..3], Is.EqualTo(msg));
+        }
+    }
+}
---
+++ EchoServer.Tests/EchoServer_CoverageTests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace EchoServer.Tests
+{
+    public class EchoServer_CoverageTests
+    {
+        [Test]
+        public async Task StartOnceAsync_ShouldAcceptClient()
+        {
+            var server = new EchoServer(6000);
+
+            var serverTask = Task.Run(() => server.StartOnceAsync());
+
+            var client = new TcpClient();
+            await client.ConnectAsync("127.0.0.1", 6000);
+
+            var stream = client.GetStream();
+            byte[] msg = { 9, 9, 9 };
+
+            await stream.WriteAsync(msg.AsMemory(0, msg.Length));
+
+            byte[] buf = new byte[100];
+            await stream.ReadAsync(buf.AsMemory(0, buf.Length));
+
+            Assert.That(server.IsRunning, Is.True);
+        }
+    }
+}
