e1f3ea942b0f4ce7dc2c5d0e5c5837a032710aa9
veronika9098351 <9098351@stud.kai.edu.ua>
2025-11-21 22:25:33 +0200

    fix6

--- EchoTspServer/EchoClientHandler.cs
+++ EchoTspServer/EchoClientHandler.cs
@@ -18,11 +18,11 @@ namespace EchoServer
             using var stream = client.GetStream();
             byte[] buffer = new byte[8192];
 
-            int bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length, token);
+            int bytesRead = await stream.ReadAsync(buffer.AsMemory(0, buffer.Length), token);
 
             var response = _processor.Process(buffer, bytesRead);
 
-            await stream.WriteAsync(response, 0, response.Length, token);
+            await stream.WriteAsync(response.AsMemory(0, response.Length), token);
         }
     }
 }
