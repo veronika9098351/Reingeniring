86717947acfc5e24c5acd6c6f98e30bf97f485e2
veronika9098351 <9098351@stud.kai.edu.ua>
2025-11-22 13:05:26 +0200

    Lab 6: Refactor EchoServer for testability and add 8 unit tests

---
+++ EchoServerTests/MessageHandlerTests.cs
@@ -0,0 +1,75 @@
+﻿using EchoServer;
+using EchoServer.Interfaces;
+using FluentAssertions;
+using Moq;
+
+namespace EchoServerTests
+{
+    [TestFixture]
+    public class MessageHandlerTests
+    {
+        private Mock<ILogger> _loggerMock;
+        private MessageHandler _handler;
+
+        [SetUp]
+        public void Setup()
+        {
+            _loggerMock = new Mock<ILogger>();
+            _handler = new MessageHandler(_loggerMock.Object);
+        }
+
+        [Test]
+        public void ProcessMessage_WithValidData_ReturnsEchoedData()
+        {
+            byte[] message = new byte[] { 0x01, 0x02, 0x03 };
+
+            var result = _handler.ProcessMessage(message);
+
+            result.Should().BeEquivalentTo(message);
+            _loggerMock.Verify(l => l.Log(It.IsAny<string>()), Times.Once);
+        }
+
+        [Test]
+        public void ProcessMessage_WithEmptyArray_ReturnsEmptyArray()
+        {
+            byte[] message = Array.Empty<byte>();
+
+            var result = _handler.ProcessMessage(message);
+
+            result.Should().BeEmpty();
+            _loggerMock.Verify(l => l.Log(It.IsAny<string>()), Times.Never);
+        }
+
+        [Test]
+        public void ProcessMessage_WithNull_ReturnsEmptyArray()
+        {
+            var result = _handler.ProcessMessage(null);
+
+            result.Should().BeEmpty();
+            _loggerMock.Verify(l => l.Log(It.IsAny<string>()), Times.Never);
+        }
+
+        [Test]
+        public void ProcessMessage_WithLargeData_ReturnsEchoedData()
+        {
+            byte[] message = new byte[1024];
+            for (int i = 0; i < message.Length; i++)
+            {
+                message[i] = (byte)(i % 256);
+            }
+
+            var result = _handler.ProcessMessage(message);
+
+            result.Should().BeEquivalentTo(message);
+        }
+
+        [Test]
+        public void Constructor_WithNullLogger_ThrowsException()
+        {
+            Action act = () => new MessageHandler(null);
+
+            act.Should().Throw<ArgumentNullException>()
+                .WithParameterName("logger");
+        }
+    }
+}
---
+++ EchoTspServer/MessageHandler.cs
@@ -0,0 +1,23 @@
+﻿namespace EchoServer
+{
+    public class MessageHandler
+    {
+        private readonly Interfaces.ILogger _logger;
+
+        public MessageHandler(Interfaces.ILogger logger)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public byte[] ProcessMessage(byte[] message)
+        {
+            if (message == null || message.Length == 0)
+            {
+                return Array.Empty<byte>();
+            }
+
+            _logger.Log($"Processing message of {message.Length} bytes");
+            return message;
+        }
+    }
+}
